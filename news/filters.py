43afe9193f5ac1de7f3835c540b37dd98dbaf159
kosumosu <kosumosu7air@gmail.com>
2021-12-04 18:00:26 +0300

    adding in PostFilter two attributes: * author * create_time to create the custom filters by author and create time.
    Deleting this fields from Meta field list.

--- news/filters.py
+++ news/filters.py
@@ -1,9 +1,17 @@
-from django_filters import FilterSet
+from django_filters import FilterSet, CharFilter, DateFilter
 from .models import Post
 
 
 # создаём фильтр
 class PostFilter(FilterSet):
+    # список полей для вывода фильтрации.
+    # на что тут влияет имя переменной я не знаю
+    # field_name - это, видимо, запрос, поле-в-модели__поле-по-ссылке__конечное-поле
+    # lookup_expr - какое выражение применять к полю (icontains == "содержит")
+    # label - текст, заголовок поля, человеческое названия для пользователя
+    author = CharFilter(field_name='author__user__username', lookup_expr='icontains', label='Пользователь')
+    create_time = DateFilter(field_name='date_time', lookup_expr='gte', label='Дата (yyyy-mm-dd)')
+
     # Здесь в мета классе надо предоставить модель и указать поля, по которым будет фильтроваться
     # (т. е. подбираться) информация о товарах
     class Meta:
@@ -13,4 +21,4 @@ class PostFilter(FilterSet):
         # я так понимаю, можно передать кортеж, а можно словарь, где уже кастомизировать фильтрацию
         # но не понятно, как, например, автора оставить также - выбором из списка
         # а рейтинг, например - сделать больше чем/меньше чем
-        fields = ['date_time', 'author']
+        fields = []
